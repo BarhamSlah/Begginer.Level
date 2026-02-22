90a753f09ca52b454762209551891fa74ac566bb
BarhamSlah <barhamsalahaddin@gmail.com>
2026-02-22 04:35:26 +0300

    Update MyFirstProject.java

--- MyFirstProject.java
+++ MyFirstProject.java
@@ -301,10 +301,20 @@ boolean isCar = false;
         ballam nabet aw double quote la la kotaye dabney 
 */
  
-// here terenary operator is begin
+/* here terenary operator is begin
+ternary operator (conditonal operator) only operator in java has 3 operand
+operand 1 ? operand 2 : operand 3  // operand 1 hamisha dabet boolean bet operand 2,3 hamisha haman jor bn ballam boolean nabn 
+ama habn manay awaya ka  booleana ( < , > , =< , >= , != , == )
+aw conditionay la operand 1 haya agar true bet O2 run dabet agar false bet O3 run dabet watta pash away 
+zanit conditionaka true yan falsea chy di esht ba conditionaka nabet 
+if bakarbena agar true w false bw chwnka agar wanakay awa rastawxo sout print dabet w awanytr karigary nabet
+*/
+ 
  int ageOfClient = 20;
- String ageText = (ageOfClient >= 18) ? "Adult" : "kid";  // tanha law kataana (") bakar bhena agar string bw wata wistt xwdy wshaka print bbet
+ String ageText = (ageOfClient >= 18) ? "Adult" : "kid";  
  System.out.println(" Our client is " + ageText);
+// tanha law katana (") bakar bena la O2 w O3 agar string bw wa la sout " nawy variable bakar bena 
+// ballam agar string nabw awa  la O3 w O2 " bakar mahena wa la sout "" bakar bhena
 
  int produceCar = 5000;
  boolean carFactory = (produceCar >= 4000) ? true : false;
@@ -323,7 +333,7 @@ boolean isCar = false;
 
 int stopWatch = 20;
  string oclock = stopWatch == 20 ? "late" : "early";
- System.out.println(oclock):
+ System.out.println(oclock);
 
   double firstValue = 20.00;
  double secondValue = 80.00;
@@ -332,6 +342,12 @@ int stopWatch = 20;
  String text = (theRemainder == 0) ? "true" : "false";
  System.out.println(text);
 
+ double A = 200.00;
+ boolean scoreCount = (A >= 150) ? true :  false ;
+ if (scoreCount) {
+System.out.println("you have got high score");
+}
+ 
 int highScore = 200;
 if(highScore > 100) { 
 highScore = 1000 + highScore;
