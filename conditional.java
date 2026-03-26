986e6329b7d083b4b051cb6d0ef1737245a1f8ed
BarhamSlah <barhamsalahaddin@gmail.com>
2026-03-26 12:21:13 +0300

    Implement basic conditional statements in Java
    Added a Java program demonstrating conditional statements and comments.

---
+++ conditional.java
@@ -0,0 +1,55 @@
+public static conditional {
+public static void main (String [] args) {
+
+ // from now on intellij begin 
+ /*
+bash nya la katy nwsyni Address w project space bakar bet labar bwny kesha w vairous la hande OS
+Upper w Lower bakardet katek 
+1 _ natwanit space bakar benit watta bo more readable
+2 _ agar la teamek kar bkait hamw dazann ka (user) variablea ballam (User) classa
+Upper camel case ( pascal case) : used for clasess and interface
+lower camel case : used for variable and functions
+ */
+
+/* la rstay if agar curly braces bakar nahenin awa conditionaka tanha bo rstay yakama watta agar conditionaka true bw 
+ awa rstay yakam run dabet ballam agar conditionaka false bet awa tanha rstay yakam run nabet w la har dw hallat 
+  rstakany tr run dabn bo away twshy le rabe awa agar tanha yak rstat habw curly braces bakar bhena */
+
+boolean isCar = false;
+ if (isCar)
+  System.out.println(" this is your car");     // labar away ka curley braces nya boya tanha aw statementa sarba baw rstay marjaya  
+ System.out.println(" this is not your car");    // labar away ka curley braces nya boya aw rsataya hamisha run dabet wakw rstayaky asayi waya
+
+boolean isCar = true;
+ if (isCar) 
+ System.out.println(" this is your car"); 
+/*
+ hokary away ka nabet la rstay if ; habet awaya ka agar falsesh bet codeaka har run dabet
+ (isCar) = (isCar == true) boya true danrawa labar away ka lz sarawa isCar = true ballam bam sheway sarawa Pro tra
+*/
+
+ boolean isCar = false;
+ if (!isCar)   //NOT operator = logical complent operator = !
+  System.out.println(" this is your car ");
+
+boolean isCar = false;
+ if (isCar = true)   // lerada hsab bo daraway kawana nakre labar away to nrxy variablet gory w tanha la boolean datwany 1 (=) dabney lanaw kawana watta assignment bkay nak comparasion  
+ System.out.println(" this is car ");
+
+ System.out.print(" barham \n is the best programmer ");     // am \n wa dakat ka codey haman line outputy line jyawaz be
+ 
+  System.out.println ("Barham " + "2026");
+  System.out.println ("Barham" + " 2026");    // am 3 code haman outputyan haya 
+  System.out.println (" Barham" +  " " + "2026 ");
+
+/*
+        int a = 20 , b = 26;
+        System.out.println(a + b):       //46
+        System.out.println("" + a + b);  //2026
+        System.out.println(" " + a + b); // 2026
+        System.out.println( a + "" + b); //2026
+        System.out.println(a + " " + b); //20 26
+        ballam nabet aw double quote la la kotaye dabney 
+*/
+  }
+}
