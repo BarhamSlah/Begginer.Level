0bf756a6a601cdcd5059fb2468b87de31d9eb274
BarhamSlah <barhamsalahaddin@gmail.com>
2026-05-25 15:09:40 +0300

    Create Scanner.java to greet user by name

---
+++ Scanner.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Bit {
+
+    public static void main (String[] args) {
+
+            Scanner names = new Scanner(System.in);
+        System.out.println("Please, Enter your name ");
+            String name  = names.nextLine();
+
+        System.out.println("Welcome " + name);
+
+
+
+            }
+        }
