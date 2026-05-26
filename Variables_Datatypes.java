7371566bb549ebf9fe8a4a518fcb6831435ed47d
BarhamSlah <barhamsalahaddin@gmail.com>
2026-05-26 14:05:02 +0300

    Update Variables_Datatypes.java

--- Variables_Datatypes.java
+++ Variables_Datatypes.java
@@ -12,6 +12,8 @@ int myFirstNumber = 1;
  int mySecondNumber = 10;
  int myThirdNumber = 100;
  System.out.println(" Hello world ");
+
+System.out.println(" \"hello World\" ");     // the output is Hello World
  
  /var   // variablea kant ba  list dadate ba pey rez bandy 
  /list  // hamw aw codeanat dadate ka nwsiwta ba pey rez bandy 
