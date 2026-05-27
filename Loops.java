e958945ed6427f1ee2ebe800659a9e581bcfb57c
BarhamSlah <barhamsalahaddin@gmail.com>
2026-05-28 00:38:19 +0300

    Add Bit class with loop examples

---
+++ Loops.java
@@ -0,0 +1,18 @@
+public class Bit {
+
+    public static void main(String[] args) {
+
+        for (int i = 10; i <= 20; i++) {
+
+            System.out.println(i);
+        }
+
+            int b = 25;
+            while (b <= 35) {
+                System.out.println(b);
+                b++;
+            }
+
+
+        }
+    }
