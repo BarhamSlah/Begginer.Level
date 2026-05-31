c192f7e38cc516e8087cd2cdbd4710077672f454
BarhamSlah <barhamsalahaddin@gmail.com>
2026-05-31 19:39:19 +0300

    Implement Bit class with addition method

---
+++ VoidMethod.java
@@ -0,0 +1,17 @@
+public class Bit {
+
+  public static void main() {
+
+      
+    addition(5,10);
+    }
+
+    public static void addition(int num1,int num2){
+
+
+      int sum = num1 + num2;
+      System.out.println("sum = " + sum);
+  }
+}
+
+
