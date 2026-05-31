d5f3742863d727fb153da7d8944dd87938b3607f
BarhamSlah <barhamsalahaddin@gmail.com>
2026-05-31 19:55:14 +0300

    Update VoidMethod.java

--- VoidMethod.java
+++ VoidMethod.java
@@ -2,16 +2,28 @@ public class Bit {
 
   public static void main() {
 
-      
     addition(5,10);
     }
 
     public static void addition(int num1,int num2){
-
 
       int sum = num1 + num2;
       System.out.println("sum = " + sum);
   }
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+public class Bit {
 
+  public static void main() {
+    
+    mutliply(21,18);
+  }
+
+  public static void mutliply(int yourAge, int myAge ){
+
+    int multiplie = myAge * yourAge;
+    System.out.println(multiplie);
+
+  }
+}
