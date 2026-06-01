a47879ea44c2e320524e375dae336f3a47db8dc9
BarhamSlah <barhamsalahaddin@gmail.com>
2026-06-01 16:15:32 +0300

    Create NotVoidMethod.java

---
+++ NotVoidMethod.java
@@ -0,0 +1,13 @@
+public class Bit {
+
+  public static void main(String[] args) {
+
+    System.out.println( "Your sum number = " + barham(12, 18));
+  }
+
+  public static int barham(int num1, int num2) {
+    int sum = num1 + num2;
+    
+    return sum;
+  }
+}
