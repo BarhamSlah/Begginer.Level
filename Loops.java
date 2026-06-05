4b706e44a7e183b559ced122ac25f0050f08cb64
BarhamSlah <barhamsalahaddin@gmail.com>
2026-06-05 15:51:12 +0300

    Update Loops.java

--- Loops.java
+++ Loops.java
@@ -1,3 +1,15 @@
+/*  
+i know you ask how would i know use for , while , do-while loop ? 
+for loop : when you exactly know that how many times repeat 
+
+while loop : when you don't konw that hoe many times repeat 
+
+do-while loop : The do-while loop is very similar to the while loop, with one major difference: it executes the code block first,
+and then checks the condition. This guarantees that the loop will run at least once, no matter what
+*/
+
+
+
 public class Bit {
 
     public static void main(String[] args) {
