18ac0cc6e1401344b9fc82e663ef8c15f1e64ffc
BarhamSlah <barhamsalahaddin@gmail.com>
2026-06-08 16:29:45 +0300

    Update Scanner.java

--- Scanner.java
+++ Scanner.java
@@ -1,16 +1,36 @@
 import java.util.Scanner;
 
-public class Bit {
-
-    public static void main (String[] args) {
-
-            Scanner names = new Scanner(System.in);
-        System.out.println("Please, Enter your name ");
-            String name  = names.nextLine();
-
-        System.out.println("Welcome " + name);
+public class a {
+    static void main(String[] args) {
 
 
+        Scanner receive = new Scanner(System.in);
 
-            }
-        }
+        System.out.print("Enter your name : ");
+        String name = receive.nextLine();
+
+        System.out.print("Enter your age : ");
+         byte age = receive.nextByte();
+         receive.nextLine();
+
+        System.out.print("Enter your height : ");
+        double height = receive.nextDouble();
+        receive.nextLine();
+
+        System.out.print("Enter your favourite color : ");
+        String color = receive.nextLine();
+
+        System.out.print("Enter your job : ");
+        String job = receive.nextLine();
+
+        System.out.print("Enter your favourite letter : " );
+        char letter = receive.next().charAt(0);
+
+        System.out.println("this is your personal info , your name is " + name + " , your age is "
+                + age + " , your height is " + height + " , your favourite color is " + color +
+                " , your job is " + job + " , your favourite color is " + color);
+
+
+receive.close();
+    }
+}
