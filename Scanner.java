858ec2b2e0252cf9100d50ba2ff067585ee7942f
BarhamSlah <barhamsalahaddin@gmail.com>
2026-06-08 17:49:29 +0300

    Update Scanner.java

--- Scanner.java
+++ Scanner.java
@@ -1,10 +1,16 @@
-import java.util.Scanner;
+import java.util.Scanner;    // that line bring Scanner to the system for receive data from user
 
 public class a {
     static void main(String[] args) {
 
 
         Scanner receive = new Scanner(System.in);
+        /*
+        Scanner is a class - constant
+        receive is a name of object - variable
+        new Scanner creator of object - constant
+        (System.in) to receive data from keyboard - constant
+         */
 
         System.out.print("Enter your name : ");
         String name = receive.nextLine();
@@ -24,13 +30,19 @@ public class a {
         String job = receive.nextLine();
 
         System.out.print("Enter your favourite letter : " );
-        char letter = receive.next().charAt(0);
+        char letter = receive.next().charAt(0);  
+        // in the primivate datatype only char written by this sort
 
         System.out.println("this is your personal info , your name is " + name + " , your age is "
                 + age + " , your height is " + height + " , your favourite color is " + color +
                 " , your job is " + job + " , your favourite color is " + color);
 
 
-receive.close();
+receive.close();   // that is make the Scanner close
     }
 }
+
+/*
+if you say how would I know when i write this line (receive.nextLine();) after all primivte datatype
+except from String beacause if you don't write this line the sout are print next to each other
+*/
