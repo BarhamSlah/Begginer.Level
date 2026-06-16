5b8954f5f0c73c9524e075c6f98c3960cda0c057
BarhamSlah <barhamsalahaddin@gmail.com>
2026-06-16 21:25:25 +0300

    Update Math&Calculation.java

--- Math&Calculation.java
+++ Math&Calculation.java
@@ -1,1 +1,17 @@
+// Constants
+System.out.println(Math.PI);
+System.out.println(Math.E);
 
+double result;
+
+// Math Methods
+result = Math.pow(2, 5);      // 32.0 (2 raised to the power of 5)
+result = Math.abs(-5);       // 5.0  (Absolute value)
+result = Math.sqrt(9);       // 3.0  (Square root)
+result = Math.round(3.14);   // 3.0  (Rounds to the nearest whole number)
+result = Math.ceil(3.14);    // 4.0  (Rounds up to the nearest integer)
+result = Math.floor(3.99);   // 3.0  (Rounds down to the nearest integer)
+result = Math.max(10, 20);   // 20.0 (Returns the larger number)
+result = Math.min(10, 20);   // 10.0 (Returns the smaller number)
+
+System.out.println(result);
