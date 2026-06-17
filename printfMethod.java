4d8d7c582963a4ece57a539026997b29f3354cd4
BarhamSlah <barhamsalahaddin@gmail.com>
2026-06-17 19:55:12 +0300

    Update printfMethod.java

--- printfMethod.java
+++ printfMethod.java
@@ -7,5 +7,6 @@
    System.out.printf("The name is %s\n " , name );
    System.out.printf("Your age is %d\n " , number);
    System.out.printf("Your fav letter is %c\n " , letter );
-   System.out.printf("Your height is %f\n " , point );
+   System.out.printf("Your height is %.2f\n " , point ); 
+   /*  aw .2 wadaka la output tanha 2 zhmaray dway point pshan bdret */
    System.out.printf("Are you student ? %b\n " , isTrue);
