0f17fe016239c083f2f0f0307994fd875be05570
BarhamSlah <barhamsalahaddin@gmail.com>
2026-06-17 15:34:59 +0300

    Update Math&Calculation.java

--- Math&Calculation.java
+++ Math&Calculation.java
@@ -1,4 +1,3 @@
-// Constants
 System.out.println(Math.PI);
 System.out.println(Math.E);
 
@@ -15,3 +14,38 @@ result = Math.max(10, 20);   // 20.0 (Returns the larger number)
 result = Math.min(10, 20);   // 10.0 (Returns the smaller number)
 
 System.out.println(result);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.util.Scanner;
+
+public class Game{
+
+public void main(String[] args) {
+
+    Scanner calculate = new Scanner (System.in);
+
+    // circumference = 2 * Math.PI * radius;
+    // area = Math.PI * Math.pow(radius, 2);
+    // volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3)
+    System.out.print("Enter the number of radius ");
+    double radius = calculate.nextDouble();
+    double circumference = 2 * Math.PI * radius;
+    System.out.println("circumference is = " + circumference + "cm");
+    
+    System.out.print("Enter the number of radius ");
+    radius = calculate.nextDouble();
+    double area = Math.PI * Math.pow(radius , 2);
+    System.out.println("Area is = " + area + "cm²");
+
+    System.out.print("Enter the numebr of radius ");
+    radius = calculate.nextDouble();
+    double volume = (4/3) * Math.PI * Math.pow(radius , 3);
+    System.out.println("volume is = " + volume + "cm³" );
+    
+calculate.close();
+}
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
