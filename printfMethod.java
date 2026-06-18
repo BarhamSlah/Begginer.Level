72033d7ba594c28d50b89c97c3fba3badcb16adf
BarhamSlah <barhamsalahaddin@gmail.com>
2026-06-18 21:16:20 +0300

    Update printfMethod.java

--- printfMethod.java
+++ printfMethod.java
@@ -1,18 +1,31 @@
- String name = "Barham";
-   int number = 18;
-   char letter = 'T';
-   boolean isTrue = true ;
-   float point = 173 ;
+import java.util.Scanner;
 
-   System.out.printf("The name is %s\n " , name );
-   System.out.printf("Your age is %d\n " , number);
-   System.out.printf("Your fav letter is %c\n " , letter );
-   System.out.printf("Your height is %.2f\n " , point ); 
-   /*  aw .2 wadaka la output tanha 2 zhmaray dway point pshan bdret */
-   System.out.printf("Are you student ? %b\n " , isTrue);
+public class Game{
 
-// (INTEGER) .X awa nya chwnka point nya & 0X zhmaraka dakata X zhmara
-/* 
-(DECIMAL) .X awanda pointa la dway 0 nishan dada & 0.X5 watta 5 zhmara dway point pshan bda w awa X chand 
- zhmara bet ba har hamwy awanda bet ba . awa awish ab 1 hisaba   
- */
+public static void main(String[] args) {
+
+Scanner bank = new Scanner (System.in);
+
+
+System.out.print("Enter the amount of money by $ ");
+double money = bank.nextDouble();
+
+System.out.print("Enter rate of bank by % ");
+double rate = bank.nextDouble() / 100;   // beacause it is percentage oyu should be divide by 100
+
+System.out.print("In one year how many repeat the rate ");
+int repeat = bank.nextInt();
+
+System.out.print("how many year do you wanna participate ? ");
+int year = bank.nextInt();
+
+ double total = money * Math.pow(1 + (rate * repeat) , repeat * year );
+
+ System.out.printf("Your money after %d year = $%.2f \n" , year , total);
+
+ System.out.println("You got " + ((total - money) / year) + "$ per year" );
+
+bank.close();
+
+}
+}
