c650214cd1ba2ad15d2bf433d10708335cd039e4
BarhamSlah <barhamsalahaddin@gmail.com>
2026-06-22 15:55:06 +0300

    Update TernaryOperator.java

--- TernaryOperator.java
+++ TernaryOperator.java
@@ -5,11 +5,11 @@ aw conditionay la operand 1 haya agar true bet O2 run dabet agar false bet O3 ru
 zanit conditionaka true yan falsea chy di esht ba conditionaka nabet 
 */
  
- int ageOfClient = 20;
- String ageText = (ageOfClient >= 18) ? "Adult" : "kid";  
+ int ageOfClient = 20;   // 1- sarata variable declare dakait 
+ String ageText = (ageOfClient >= 18) ? "Adult" : "kid";  // 2- variable lanaw condition dadaney 
+  // 3 - variabley dwam ba ch declare krabw dabe 2 anjamaka ba pey aw datatype bet
  System.out.println(" Our client is " + ageText);
-// tanha law katana (") bakar bena la O2 w O3 agar string bw wa la sout " nawy variable bakar bena 
-// ballam agar string nabw awa  la O3 w O2 " bakar mahena wa la sout "" bakar bhena
+
 
  int produceCar = 5000;
  boolean carFactory = (produceCar >= 4000) ? true : false;
@@ -17,18 +17,22 @@ zanit conditionaka true yan falsea chy di esht ba conditionaka nabet
   System.out.println(" We have a lot of Car");
  }
 
+
  String makeOfCar = "Toyota";
  boolean isDomestic = makeOfCar == "Toyota" ? true : false;  // boolean isDomestic = makeOfCar == ""Toyota" ? false : true;   =  boolean isDomestic = ( makeOfCar != "Toyota" );                                                  
  if (isDomestic) {
   System.out.println(" Toyota is your car");
  }
 
+
  String yourCar =  (isDomestic) ? "this is your car" : "this is not your car";
  System.out.println( carModel );
+
 
 int stopWatch = 20;
  string oclock = (stopWatch >= 20) ? "late" : "early";
  System.out.println(oclock);
+
 
   double firstValue = 20.00;
  double secondValue = 80.00;
