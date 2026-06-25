a6702798e510827f51a6acac55104ab2c2ffad05
BarhamSlah <barhamsalahaddin@gmail.com>
2026-06-25 15:04:28 +0300

    Update method.java

--- method.java
+++ method.java
@@ -198,5 +198,28 @@ void print(int... a) { System.out.println("گۆڕاو"); }
 
   لە چاوپێکەوتنی کاردا حەز دەکەن وشە ئەکادیمییەکان ببیستن.
    Method Overloading دەوترێت Compile-time Polymorphism (یاخود Static Binding). کاتێک دەپرسن بۆچی؟ بڵێ: چونکە کۆمپایلەر لە کاتی دروستکردنی کۆدەکەدا بڕیار دەدات کام مێتۆد بەکاربهێنرێت، نەک لە کاتی کارپێکردنیدا
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  public class GuessNumber {
+    public static void main(String[] args) {
 
-  
+        System.out.println(calculate(12.0, 23.0));   // labar away 1 method 2 parametery haay keshay nya 
+        System.out.println(calculate(12.0, 23, 23.0));   // lera labar away 2 method 3 parametery haya boya dabet dyari bkay inta yan double
+        System.out.println(calculate(12 , 23.0 , 2.0 ));
+    }
+
+    public static double calculate(double num1, double num2) {
+        double sum = num1 + num2;
+        return sum;itn
+    }
+
+    public static double calculate(double num1, int num2, double num3) {
+        double sum = num1 + num2 - num3;
+        return sum;
+    }
+
+    public static double calculate(int num1 , double num2 , double num3) {
+        double sum = num1 + num2 * num3;
+        return sum;
+}
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
