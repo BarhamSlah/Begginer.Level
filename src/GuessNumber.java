c96b03b0091aec062ac504d935e997f9469cd821
Barham Slah <BarhamSlah@gmail.com>
2026-06-25 14:00:49 +0300

    first method

---
+++ src/GuessNumber.java
@@ -0,0 +1,14 @@
+public class GuessNumber {
+    public static void main(String[] args)  {
+
+        System.out.println (calculate(10 , 12));
+
+
+       }
+
+       public static double calculate(double num1 , double num2){
+       double sum = num1 + num2;
+           return sum;
+       }
+
+    }
