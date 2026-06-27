700ea1a3cb9d1e08a416b618ed0f08998614a5df
BarhamSlah <barhamsalahaddin@gmail.com>
2026-06-27 19:27:49 +0300

    Update Git.java

--- Git.java
+++ Git.java
@@ -1,1 +1,18 @@
+pwd // pet dalle al kwey 
 
+ls  // law shenay ka leyma chy haya
+
+cd (url) / (name) // awam lo bkawa  
+
+cd ../  // garanawa bo shewny peshw
+
+cd  // datgarenetawa bo sarata
+
+mkdir (name)  // bo drwst krdny folder
+
+touch (name).txt / java / python / jpg  // bo drwst krdny file
+
+  
+
+  
+  
