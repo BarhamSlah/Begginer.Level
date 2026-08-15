862a49c65e2a9393eec46c14cd0ed2370ccf2f18
BarhamSlah <barhamsalahaddin@gmail.com>
2026-08-15 12:35:49 +0300

    Add nested if example for card validation
    Added example of nested if statements for card processing logic.

--- NestedIf.java
+++ NestedIf.java
@@ -1,3 +1,23 @@
+Independent Conditions (when conditions are not directly dependent on each other): Use if / else if or &&.
+Dependent Conditions (when the inner condition makes no sense without the outer condition being true): Use nested if.
+    
+****************************************************************************************************************************
+// نموونەی دروست بۆ nested if:
+if (isCardInserted) {
+    // ئەم بەشە تەنها کاتێک پشکنینی بۆ دەکرێت کە کارتەکە داخل کرابێت
+    if (isPinCorrect) {
+        // ئەم بەشەش تەنها کاتێک ڕەن دەبێت کە کۆدەکە ڕاست بێت
+        if (balance >= amount) {
+            System.out.println("پارەکە ڕاکێشرا");
+        } else {
+            System.out.println("پارەی پێویست لە هەژمارەکەتدا نییە");
+        }
+    } else {
+        System.out.println("کۆدی کارتەکە هەڵەیە");
+    }
+}
+
+*****************************************************************************************************************************
 void main(String[] args) {
 
     boolean isUnemployee = false;
