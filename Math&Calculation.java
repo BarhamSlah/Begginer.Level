51f0fe5154d671d44c7f62cbc2308f9a9579cd4d
Barham Salahaddin <barhamsalahaddin@gmail.com>
2026-09-09 16:57:45 +0300

    Update Math&Calculation.java

--- Math&Calculation.java
+++ Math&Calculation.java
@@ -1,3 +1,6 @@
+If the operator calculates a value (+, -, *, /), it goes Left to Right.
+If the operator stores/assigns a value (=, +=, -=), it goes Right to Left.
+
 System.out.println(Math.PI);
 System.out.println(Math.E);
 
