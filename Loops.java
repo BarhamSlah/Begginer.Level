45ec345af06f9f54a4fd9756e5e4899051e41db5
Barham Salahaddin <barhamsalahaddin@gmail.com>
2026-09-17 21:54:33 +0300

    Update Loops.java

--- Loops.java
+++ Loops.java
@@ -93,4 +93,20 @@ public class GuessNumber {
 }
 ////////////////////////////////////////////////////////////////////////////////////
 
+public class Main {
+    public static void main(String[] args) {
 
+        int[] numbers = {10, 20, 30, 40};
+
+        for (int number : numbers) {
+            System.out.println(number);
+
+        /*
+        for (Type variable : array) {
+        // code
+        }
+        */
+          
+        }
+    }
+}
